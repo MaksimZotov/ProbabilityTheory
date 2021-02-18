0361554db180ecdd17b3f0e2d958e08059ba849f
MaksimZotov <zotovmaksim1254@gmail.com>
2021-02-19 00:00:26 +0300

    code style

--- sources/helpers/NumberGenerator.cpp
+++ sources/helpers/NumberGenerator.cpp
@@ -2,11 +2,15 @@
 #include "../../headers/helpers/NumberGenerator.h"
 
 int GetRandomInt(int a, int b) {
-    if (a >= b) throw "a must be less than b";
+    if (a >= b)
+        throw "a must be less than b";
+
     return rand() % (b - a + 1) + a;
 }
 
 double GetRandomDouble(double a, double b) {
-    if (a > b) throw "a must be less than b or equal to b";
+    if (a > b)
+        throw "a must be less than b or equal to b";
+
     return (a == b) ? 0 : ((b - a) * ((double)rand() / RAND_MAX)) + a;
 }
