88914abce1db9ce534bc92163728ba0d59bf059d
MaksimZotov <zotovmaksim1254@gmail.com>
2021-02-18 08:03:10 +0300

    refactored task_2_14

--- sources/tasks/Task_2_14.cpp
+++ sources/tasks/Task_2_14.cpp
@@ -13,8 +13,10 @@ void Task_2_14::ShowResults(double* results, int n)
 void Task_2_14::Start(int numberOfIterations, int numberOfTickets, int numberOfTakenTickets,
                       int numberOfWinningTickets, int numberOfResults, void (*CountFavorableOutcomes) (int*, int))
 {
-    if (numberOfTickets < numberOfWinningTickets) throw "number of tickets must not be less than number of winning tickets";
-    if (numberOfTickets < numberOfTakenTickets) throw "number of tickets must not be less than number of taken tickets";
+    if (numberOfTickets < numberOfWinningTickets)
+        throw "number of tickets must not be less than number of winning tickets";
+    if (numberOfTickets < numberOfTakenTickets)
+        throw "number of tickets must not be less than number of taken tickets";
 
     int *countOfFavorableOutcomes = new int[numberOfResults];
     InitArrayWithZeroes(countOfFavorableOutcomes, numberOfResults);
