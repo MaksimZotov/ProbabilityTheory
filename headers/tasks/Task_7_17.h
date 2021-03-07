2705f27b750fce47ba1a071a1b1590f3388b4f40
MaksimZotov <zotovmaksim1254@gmail.com>
2021-03-07 15:00:05 +0300

    fix

--- headers/tasks/Task_7_17.h
+++ headers/tasks/Task_7_17.h
@@ -1,6 +1,6 @@
 #ifndef PROBABILITYTHEORY_TASK_7_17_H
 #define PROBABILITYTHEORY_TASK_7_17_H
 
-void StartTask_7_17(int numberOfIterations, int numberOfButches, double percentOfSecondRate);
+void StartTask_7_17(int numberOfIterations, int numberOfBatches, double percentOfSecondRate);
 
 #endif
