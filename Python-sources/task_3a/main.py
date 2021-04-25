b550a456c039a9908f01acb2d483c73db9b62dce
MaksimZotov <zotovmaksim1254@gmail.com>
2021-04-26 01:57:04 +0300

    task_3a

--- Python-sources/task_3a/main.py
+++ Python-sources/task_3a/main.py
@@ -4,6 +4,19 @@ import numpy as np
 import copy
 import random
 import re
+import math
+
+
+def k(x):
+    return math.exp(- x * x / 2) / math.sqrt(2 * math.pi)
+
+
+def p_KDE(x):
+    sigma = 0
+    for i in range(N):
+        sigma += k((x - listOfValuesFloat[i]) / h_opt)
+    return sigma / (N * h_opt)
+
 
 parts = 10
 
@@ -14,7 +27,6 @@ with open('Python-sources/task_3a/Task_3a.txt') as line:
     listOfValuesFloat = []
     for i in range(N):
         listOfValuesFloat.append(float(listOfValuesStr[i]))
-    debug = 0
     random.shuffle(listOfValuesFloat)
     listsParts = []
     part = 0
@@ -29,6 +41,16 @@ with open('Python-sources/task_3a/Task_3a.txt') as line:
             count = 0
     listOfValuesFloat.sort()
 
+    # Определение мат. ожидания
+    M = listOfValuesFloat[int(N / 2)]
+
+    # Поиск дисперсии
+    sigma = 0
+    for i in range(N):
+        sigma += (M - listOfValuesFloat[i]) ** 2
+    D = sigma / N
+
+    # Функция распределения
     F = []
     x_for_F = []
     for i in range(N):
@@ -37,6 +59,7 @@ with open('Python-sources/task_3a/Task_3a.txt') as line:
         F.append(i / N)
         F.append((i + 1) / N)
 
+    # Функция плотности распределения
     m = N / 300
     interval = (max(listOfValuesFloat) - min(listOfValuesFloat)) / m
     index = 0
@@ -49,12 +72,20 @@ with open('Python-sources/task_3a/Task_3a.txt') as line:
         curCount += 1
         if listOfValuesFloat[i] > curMax:
             for j in range(curCount):
-                f.append(curCount)
+                f.append(curCount / N)
             curCount = 0
             curMax += interval
     if curCount > 0:
         for j in range(curCount):
-            f.append(curCount)
+            f.append(curCount / N)
+
+    # Определение h_opt
+    h_opt = 1.06 * math.sqrt(D) * (N ** (- 1 / 5))
+
+    # Ядерная оценка плотности
+    f_KDE = []
+    for value in listOfValuesFloat:
+        f_KDE.append(p_KDE(value))
 
 fig1 = go.Figure()
 fig1.add_trace(go.Scatter(x=x_for_F, y=F))
@@ -64,4 +95,6 @@ fig2 = go.Figure()
 fig2.add_trace(go.Scatter(x=x_for_f, y=f))
 fig2.show()
 
-debug = 0
+fig3 = go.Figure()
+fig3.add_trace(go.Scatter(x=listOfValuesFloat, y=f_KDE))
+fig3.show()
