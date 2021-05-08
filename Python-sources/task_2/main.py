d65d90a3305e571303600c4efaabb4deb0e15069
MaksimZotov <zotovmaksim1254@gmail.com>
2021-05-08 22:03:47 +0300

    task_2

--- Python-sources/task_2/main.py
+++ Python-sources/task_2/main.py
@@ -122,7 +122,7 @@ figure.show()
 p_get_prize_list_y = [p_get_prize]
 p_get_prize_list_x = [1]
 for n in range(2, 501):
-    p_get_prize_list_y.append((p_get_prize) ** n)
+    p_get_prize_list_y.append((1 - p_get_prize) ** (n - 1) * p_get_prize)
     p_get_prize_list_x.append(n)
 figure = go.Figure()
 figure.add_trace(go.Scatter(x=p_get_prize_list_x, y=p_get_prize_list_y))
@@ -169,6 +169,7 @@ def create_matrix_variants(matrix, matrix_variants, wild_indexes, symbols_si, si
             matrix_variant[wild_indexes[s][0]][wild_indexes[s][1]] = si_iter[s]
         matrix_variants.append(matrix_variant)
 
+
 symbols_si = ['S1', 'S2', 'S3', 'S4', 'S5', 'S6', 'S7', 'S8']
 
 matrix = [['any', 'any', 'any'],
@@ -176,6 +177,133 @@ matrix = [['any', 'any', 'any'],
           ['any', 'any', 'any'],
           ['any', 'any', 'any'],
           ['any', 'any', 'any']]
+
+p_win_dict = {'line_1': {}, 'line_2': {}, 'line_3': {}, 'line_4': {}, 'line_5': {}}
+line_n_1 = 1
+line_n_2 = 1
+line_n_3 = 1
+line_n_4 = 1
+line_n_5 = 1
+for i in range(10000):
+    create_matrix(matrix)
+    wild_indexes = []
+    for i in range(columns):
+        for j in range(rows):
+            if matrix[i][j] == 'Wild':
+                wild_indexes.append([i, j])
+    matrix_variants = []
+    create_matrix_variants(matrix, matrix_variants, wild_indexes, symbols_si, [])
+    prize_1 = 0
+    prize_2 = 0
+    prize_3 = 0
+    prize_4 = 0
+    prize_5 = 0
+    for matrix_variant in matrix_variants:
+        line_1 = [matrix_variant[0][0], matrix_variant[1][0], matrix_variant[2][0], matrix_variant[3][0], matrix_variant[4][0]]
+        line_2 = [matrix_variant[0][1], matrix_variant[1][1], matrix_variant[2][1], matrix_variant[3][1], matrix_variant[4][1]]
+        line_3 = [matrix_variant[0][2], matrix_variant[1][2], matrix_variant[2][2], matrix_variant[3][2], matrix_variant[4][2]]
+        line_4 = [matrix_variant[0][0], matrix_variant[1][1], matrix_variant[2][2], matrix_variant[3][1], matrix_variant[4][0]]
+        line_5 = [matrix_variant[0][2], matrix_variant[1][1], matrix_variant[2][0], matrix_variant[3][1], matrix_variant[4][2]]
+        cur_prize_1 = get_prize_from_line(line_1)
+        cur_prize_2 = get_prize_from_line(line_2)
+        cur_prize_3 = get_prize_from_line(line_3)
+        cur_prize_4 = get_prize_from_line(line_4)
+        cur_prize_5 = get_prize_from_line(line_5)
+        x = 0
+        for i in range(len(matrix_variant)):
+            for j in range(len(matrix_variant[i])):
+                if matrix_variant[i][j] == 'Scatter':
+                    x += 1
+        if x >= 2:
+            if x == 2:
+                cur_prize_1 *= scatter_to_x['scatter_2']
+                cur_prize_2 *= scatter_to_x['scatter_2']
+                cur_prize_3 *= scatter_to_x['scatter_2']
+                cur_prize_4 *= scatter_to_x['scatter_2']
+                cur_prize_5 *= scatter_to_x['scatter_2']
+            elif x == 3:
+                cur_prize_1 *= scatter_to_x['scatter_3']
+                cur_prize_2 *= scatter_to_x['scatter_3']
+                cur_prize_3 *= scatter_to_x['scatter_3']
+                cur_prize_4 *= scatter_to_x['scatter_3']
+                cur_prize_5 *= scatter_to_x['scatter_3']
+            elif x == 4:
+                cur_prize_1 *= scatter_to_x['scatter_4']
+                cur_prize_2 *= scatter_to_x['scatter_4']
+                cur_prize_3 *= scatter_to_x['scatter_4']
+                cur_prize_4 *= scatter_to_x['scatter_4']
+                cur_prize_5 *= scatter_to_x['scatter_4']
+            elif x == 5:
+                cur_prize_1 *= scatter_to_x['scatter_greater_4']
+                cur_prize_2 *= scatter_to_x['scatter_greater_4']
+                cur_prize_3 *= scatter_to_x['scatter_greater_4']
+                cur_prize_4 *= scatter_to_x['scatter_greater_4']
+                cur_prize_5 *= scatter_to_x['scatter_greater_4']
+        if cur_prize_1 > prize_1: prize_1 = cur_prize_1
+        if cur_prize_2 > prize_2: prize_2 = cur_prize_2
+        if cur_prize_3 > prize_3: prize_3 = cur_prize_3
+        if cur_prize_4 > prize_4: prize_4 = cur_prize_4
+        if cur_prize_5 > prize_5: prize_5 = cur_prize_5
+    if prize_1 + prize_2 + prize_3 + prize_4 + prize_5 != 0:
+        debug = 0
+
+    if prize_1 > 0:
+        if p_win_dict['line_1'].__contains__(line_n_1): p_win_dict['line_1'][line_n_1] += 1
+        else: p_win_dict['line_1'][line_n_1] = 1
+        line_n_1 = 1
+    else:
+        line_n_1 += 1
+    if prize_2 > 0:
+        if p_win_dict['line_2'].__contains__(line_n_2): p_win_dict['line_2'][line_n_2] += 1
+        else: p_win_dict['line_2'][line_n_2] = 1
+        line_n_2 = 1
+    else:
+        line_n_2 += 1
+    if prize_3 > 0:
+        if p_win_dict['line_3'].__contains__(line_n_3): p_win_dict['line_3'][line_n_3] += 1
+        else: p_win_dict['line_3'][line_n_3] = 1
+        line_n_3 = 1
+    else:
+        line_n_3 += 1
+    if prize_4 > 0:
+        if p_win_dict['line_4'].__contains__(line_n_4): p_win_dict['line_4'][line_n_4] += 1
+        else: p_win_dict['line_4'][line_n_4] = 1
+        line_n_4 = 1
+    else:
+        line_n_4 += 1
+    if prize_5 > 0:
+        if p_win_dict['line_5'].__contains__(line_n_5): p_win_dict['line_5'][line_n_5] += 1
+        else: p_win_dict['line_5'][line_n_5] = 1
+        line_n_5 = 1
+    else:
+        line_n_5 += 1
+
+p_win_dict_new_keys = {'line_1': {}, 'line_2': {}, 'line_3': {}, 'line_4': {}, 'line_5': {}}
+for line_p_win in p_win_dict:
+    p_win_dict_new_keys[line_p_win] = sorted(p_win_dict[line_p_win])
+
+p_win_dict_new_keys_and_values = {'line_1': {}, 'line_2': {}, 'line_3': {}, 'line_4': {}, 'line_5': {}}
+for line in p_win_dict_new_keys:
+    for n in p_win_dict_new_keys[line]:
+        p_win_dict_new_keys_and_values[line][n] = p_win_dict[line][n]
+
+acc = {'line_1': 0, 'line_2': 0, 'line_3': 0, 'line_4': 0, 'line_5': 0}
+for line in acc:
+    for n in p_win_dict_new_keys_and_values[line]:
+        acc[line] += p_win_dict_new_keys_and_values[line][n]
+
+axis_x = {'line_1': [], 'line_2': [], 'line_3': [], 'line_4': [], 'line_5': []}
+axis_y = {'line_1': [], 'line_2': [], 'line_3': [], 'line_4': [], 'line_5': []}
+for line in acc:
+    for n in p_win_dict_new_keys_and_values[line]:
+        axis_y[line].append(p_win_dict_new_keys_and_values[line][n] / acc[line])
+        axis_x[line].append(n)
+
+figure = go.Figure()
+for line in acc:
+    figure.add_trace(go.Scatter(x=axis_x[line], y=axis_y[line]))
+figure.show()
+
 
 prize_list_1 = []
 prize_list_2 = []
